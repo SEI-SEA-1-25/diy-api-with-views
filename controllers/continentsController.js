0264d4407c8b2a99f0157e2d6b4a98e730557e93
Jcom619 <joshuacompton@my.smccd.edu>
2021-02-24 08:59:32 -0800

    JJC

--- controllers/continentsController.js
+++ controllers/continentsController.js
@@ -1,13 +1,22 @@
 const router = require('express').Router()
-
 const models = require('../models')
+//              //               //
 
+
+//              //               //
+//      GET    │ /continents     //
+//              //               //
 router.get('/', (req, res) => {
   models.continent.findAll().then((continents) => {
     res.json({ continents })
   })
 })
+//              //               //
 
+
+//              //               //
+//     POST   │ /continents      //
+//              //               //
 router.post('/', (req, res) => {
   models.continent.create({
     name: req.body.name,
@@ -15,16 +24,25 @@ router.post('/', (req, res) => {
     res.json({continent})
   })
 })
+//              //               //
 
+
+//              //              //
+// GET    │ /continents/:id     //
+//              //              //
 router.get('/:id', (req, res) => {
   models.findByPk(req.params.id).then((continent => {
     res.json({ continent })
   }))
 })
+//              //               //
 
+
+//                    //                  //
+//    POST   │ /continents/:id/countries  //
+//                    //                  //
 router.post('/:id/countries', async (req, res) => {
   const continent = await models.continent.findByPk(req.params.id)
-
   const country = await continent.createCountry({
     name: req.body.name,
     founded: req.body.founded,
@@ -33,7 +51,12 @@ router.post('/:id/countries', async (req, res) => {
 
   res.json({country})
 })
+//              //               //
 
+
+//                 //                 //
+// GET   │ /continents/:id/countries  //
+//                 //                 //
 router.get('/:id/countries', async (req, res) => {
   const continent = await models.continent.findByPk(req.params.id)
 
@@ -41,5 +64,8 @@ router.get('/:id/countries', async (req, res) => {
 
   res.json({ countries })
 })
+//              //               //
+
+
 
 module.exports = router
