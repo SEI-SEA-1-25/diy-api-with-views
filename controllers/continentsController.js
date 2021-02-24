e28ea9eef0e5eff85161ece4f9f101fafa910f22
dlashinsky <da.lashinsky@gmail.com>
2021-02-24 01:11:07 -0800

    finishing HW

--- controllers/continentsController.js
+++ controllers/continentsController.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-
 const models = require('../models')
 
 router.get('/', (req, res) => {
--- controllers/countriesController.js
+++ controllers/countriesController.js
@@ -2,63 +2,75 @@ const router = require('express').Router()
 
 const models = require('../models')
 
-router.get('/', (req, res) => {
-  models.country.findAll().then((countries) => {
-    res.json({ countries })
-  })
-  .catch((error) => {
+
+//Finding all countries
+router.get('/', async (req, res) => {
+  try{
+      const countries = await models.country.findAll({ raw: true })
+ 
+    res.render('countries/index', { countries })
+  } catch(error) {
     res.json({ error })
-  })
+  } 
 })
 
-router.post('/', (req, res) => {
-  models.country.create({
-    name: req.body.name,
-    founded: req.body.founded,
-    population: req.body.population
-  }).then((country) => {
-    res.json({ country })
-  })
-  .catch((error) => {
-    res.json({ error })
-  })
+
+// To render the New.ejs file properly when clicking the link 
+router.get('/new', (req, res) => {
+  res.render("countries/new")
 })
 
+
+//Creating a new country and subsequent data
+router.post('/', async (req, res) => {
+    try{
+      const newCountry = await models.country.create({
+        name: req.body.name,
+        founded: req.body.founded,
+        population: req.body.population
+      })
+      res.redirect(`/countries/${newCountry.id}`)
+    }catch(error){
+      console.log(error)
+    }
+})
+
+//Show Single Country
 router.get('/:id', async (req, res) => {
   try {
-    const country = await models.country.findByPk(req.params.id)
-    res.json({ country })
+    const country = await models.country.findByPk(req.params.id, { raw: true })
+    res.render('countries/show', { country })
   } catch (error) {
     res.json({ error })
   }
 })
 
+//Update info on a single country
 router.put('/:id', async (req, res) => {
   try {
-    await models.country.update(req.body, {
-      where: {
-        id: req.params.id
-      }
-    })
-
     const country = await models.country.findByPk(req.params.id)
-    res.json({ country })
+    const updatedCountry = await country.update({
+      name: req.body.name,
+      founded: req.body.founded,
+      population: req.body.population
+    })
+    
+    res.redirect(`/countries/${req.params.id}`)
   } catch (error) {
-    res.json({ error })    
+    console.log(error)  
   }
 })
 
-router.delete('/:id', (req, res) => {
-  models.country.destroy({
-    where: { id: req.params.id }
-  })
-  .then((country) => {
-    res.json({ country })
-  })
-  .catch((err) => {
-    res.json({ error })
-  })
-})
+//Delete a country
+router.delete('/:id', async (req, res) => {
+  try{
+    const country = await models.country.findByPk(req.params.id)
+    const deletedCountry = await country.destroy();
+    res.redirect('/countries')
+  }catch(error){
+    console.log(error)
+  }
 
+})
 
 module.exports = router
