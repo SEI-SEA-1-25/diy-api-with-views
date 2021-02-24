e7016b30eeeb875fb83ff338e7ac4094fc00ef53
ChrisB007 <chrisbitoy@gmail.com>
2021-02-24 12:04:50 -0500

    lates on the diy

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
-const express = require('express')
-const app = express()
-const rowdy = require('rowdy-logger')
+const express = require('express');
+const ejsLayouts = require('express-ejs-layouts');
+const app = express();
+const rowdy = require('rowdy-logger');
+const methodOverride = require("method-override");
 
-const rowdyRes = rowdy.begin(app)
+const rowdyRes = rowdy.begin(app);
 
 
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(ejsLayouts);
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
-  res.send('hello from root!')
+  res.render('index', {title: "Welcome"})
 })
 
 app.use('/countries', require('./controllers/countriesController'))
 app.use('/continents', require('./controllers/continentsController'))
+
+app.get('/continents', (req, res)=>{
+  res.render('index', {title: "Continents"})
+})
+
+app.get('/countries', (req, res)=>{
+  res.render('index', {title: 'Countries'})
+
+})
+
+
+
+
+
+
 
 
 
