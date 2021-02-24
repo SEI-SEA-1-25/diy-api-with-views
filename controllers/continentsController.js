a482d8e4359dff2dc524b698095cd0e21cbe0643
AlrightErin27 <erinmontybruce@gmail.com>
2021-02-23 19:19:23 -0800

    info all here, css not working

--- controllers/continentsController.js
+++ controllers/continentsController.js
@@ -1,45 +1,123 @@
-const router = require('express').Router()
+// const router = require("express").Router();
+// const models = require("../models");
 
-const models = require('../models')
+// //READ ALL
+// router.get("/", (req, res) => {
+//   models.continent.findAll().then((continents) => {
+//     res.render({ continents });
+//   });
+// });
 
-router.get('/', (req, res) => {
-  models.continent.findAll().then((continents) => {
-    res.json({ continents })
-  })
-})
+// //Create?
+// router.post("/", (req, res) => {
+//   models.continent
+//     .create({
+//       name: req.body.name,
+//     })
+//     .then((continent) => {
+//       res.render({ continent });
+//     });
+// });
 
-router.post('/', (req, res) => {
-  models.continent.create({
-    name: req.body.name,
-  }).then((continent) => {
-    res.json({continent})
-  })
-})
+// //New Continent Form Route
+// router.get("/:id", (req, res) => {
+//   models.findByPk(req.params.id).then((continent) => {
+//     res.render({ continent });
+//   });
+// });
 
-router.get('/:id', (req, res) => {
-  models.findByPk(req.params.id).then((continent => {
-    res.json({ continent })
-  }))
-})
+// //Create New Continent
+// router.post("/:id/countries", async (req, res) => {
+//   const continent = await models.continent.findByPk(req.params.id);
 
-router.post('/:id/countries', async (req, res) => {
-  const continent = await models.continent.findByPk(req.params.id)
+//   const country = await continent.createCountry({
+//     name: req.body.name,
+//     founded: req.body.founded,
+//     population: req.body.population,
+//   });
 
-  const country = await continent.createCountry({
-    name: req.body.name,
-    founded: req.body.founded,
-    population: req.body.population,
-  })
+//   res.render({ country });
+// });
 
-  res.json({country})
-})
+// //Read One
+// router.get("/:id/countries", async (req, res) => {
+//   const continent = await models.continent.findByPk(req.params.id);
 
-router.get('/:id/countries', async (req, res) => {
-  const continent = await models.continent.findByPk(req.params.id)
+//   const countries = await continent.getCountries();
 
-  const countries = await continent.getCountries()
+//   res.render({ countries });
+// });
 
-  res.json({ countries })
-})
+// module.exports = router;
+const router = require("express").Router();
+const db = require("../models");
 
-module.exports = router
+// CRUD routes for continents
+// Index (Read All) Route
+router.get("/", async (req, res) => {
+  try {
+    const continents = await db.continent.findAll({ raw: true });
+    // console.log(continents)
+    // res.send()
+    res.render("continents/index", { continents });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// New continent Form Route
+router.get("/new", (req, res) => {
+  res.render("continents/new");
+});
+
+// Show (Show One) Route
+router.get("/:id", async (req, res) => {
+  try {
+    const continent = await db.continent.findByPk(req.params.id, { raw: true });
+    console.log(continent);
+    res.render("continents/show", { continent });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Create Route
+router.post("/", async (req, res) => {
+  try {
+    // console.log(req.body);
+    const newContinent = await db.continent.create({
+      name: req.body.name,
+      size: req.body.size,
+    });
+    res.redirect(`/continents/${newContinent.id}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Update route
+router.put("/:id", async (req, res) => {
+  try {
+    const continent = await db.continent.findByPk(req.params.id);
+    const updatedContinent = await continent.update({
+      name: req.body.name,
+      size: req.body.size,
+    });
+    res.redirect(`/continents/${req.params.id}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Delete route
+router.delete("/:id", async (req, res) => {
+  try {
+    const continent = await db.continent.findByPk(req.params.id);
+    const deletedContinent = await continent.destroy();
+    res.redirect("/continents");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-const express = require('express')
-const app = express()
-const rowdy = require('rowdy-logger')
+// Required Modules
+const express = require("express");
+const rowdy = require("rowdy-logger");
+const db = require("./models");
+const ejsLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 
-const rowdyRes = rowdy.begin(app)
+// Variables
+const app = express();
+const PORT = 3000;
+const rowdyResults = rowdy.begin(app);
 
+// Middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(ejsLayouts);
+app.use(express.urlencoded({ extended: false })); // urlencoded gets the form data from the request and puts it inside of req.body
+app.use(methodOverride("_method"));
 
+// //controllers
+app.use("/countries", require("./controllers/countriesController"));
+app.use("/continents", require("./controllers/continentsController"));
 
-app.use(express.urlencoded({ extended: false }))
+// Routes
+app.get("/", (req, res) => {
+  res.send("hello from root!");
+});
+// app.get("./views/anotherpage.ejs", (req, res) => {
+//   res.render("./views/anotherpage.ejs");
+// });
 
-app.get('/', (req, res) => {
-  res.send('hello from root!')
-})
-
-app.use('/countries', require('./controllers/countriesController'))
-app.use('/continents', require('./controllers/continentsController'))
-
-
-
-const PORT = process.env.PORT || 3000
+// Start the server!
 app.listen(PORT, () => {
-  console.log('server started!');
-  rowdyRes.print()
-})
+  rowdyResults.print();
+  console.log(`Server is listening on port ${PORT}`);
+});
